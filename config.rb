302c59a7bd4c3d39683cbacd91a0c9b33777a5ab
Chris Tulip <Chris@wearebrandnew.com>
2013-09-05 12:12:30 -0700

    added a cname file and preped for deploy

--- config.rb
+++ config.rb
@@ -7,6 +7,6 @@ set :images_dir, 'images'
 activate :deploy do |deploy|
   deploy.method = :git
   # Optional Settings
-  deploy.remote = "ctulip.me" # remote name or git url, default: origin
+  # deploy.remote = "ctulip.me" # remote name or git url, default: origin
   # deploy.branch = "custom-branch" # default: gh-pages
 end
