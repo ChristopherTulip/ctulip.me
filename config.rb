f6815c671ccc562620622a653ebf08087abfd542
Chris Tulip <Chris@wearebrandnew.com>
2013-09-05 14:16:54 -0700

    update to the title and some other minor changes

--- config.rb
+++ config.rb
@@ -54,17 +54,12 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
+end
 
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
+activate :deploy do |deploy|
+  deploy.method = :git
 end
